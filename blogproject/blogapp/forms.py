ae94c97a12648277ff96238a10c0db926fa2b5da
TroetschDaniel <danartur0307@hotmail.com>
2025-05-18 23:55:22 -0500

    implementacion de las etiquetas

--- blogproject/blogapp/forms.py
+++ blogproject/blogapp/forms.py
@@ -16,12 +16,9 @@ class BlogForm(forms.ModelForm):
     )
 
     tags = forms.ModelMultipleChoiceField(
-        queryset=Tag.objects.none(),  # inicializa vacío
-        widget=forms.CheckboxSelectMultiple(
-            attrs={'class': 'checkbox-tag flex flex-wrap gap-4'}
-        ),
+        queryset=Tag.objects.all(),
         required=False,
-        label="Etiquetas"
+        widget=forms.MultipleHiddenInput  # El widget no afecta mucho porque lo manejas tú
     )
 
     class Meta:
@@ -35,6 +32,7 @@ class BlogForm(forms.ModelForm):
             'image': forms.FileInput(attrs={
                 'class': 'block w-full text-sm text-gray-400 file:mr-4 file:py-2 file:px-4 file:rounded-lg file:border-0 file:text-sm file:font-medium file:bg-neon file:text-black hover:file:bg-neon/90'
             }),
+            'tags': forms.MultipleHiddenInput(),
         }
         labels = {
             'title': 'Título',
@@ -48,7 +46,7 @@ class BlogForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['content'].initial = 'Escribe aquí tu contenido...'
         self.fields['tags'].queryset = Tag.objects.all()
-        self.fields['tags'].widget = forms.HiddenInput()  # ← oculta el campo
+        #self.fields['tags'].widget = forms.HiddenInput()  # ← oculta el campo
 
 
 class ReviewForm(forms.ModelForm):
